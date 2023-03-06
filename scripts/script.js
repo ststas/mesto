22231be10b6e0e6d64dc777f09c031496b4778cb
Stas Zaitsev <ststas@yandex.ru>
2023-03-06 15:58:29 +0200

    feat: to update javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,17 @@
 let popup = document.querySelector('.popup');
-popup.classList.add('popup_opened');
-console
-// let edit_button = document.querySelector('profile__edit-button');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+
+function popupOpen() {
+  popup.classList.add('popup_opened');
+  console.log('мы открыли popup');
+}
+
+function popupClose() {
+  popup.classList.remove('popup_opened');
+  console.log('мы закрыли popup');
+}
+
+
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
