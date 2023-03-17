40718f4e24fd935a27ed5a8f375001e86d22998f
Stas Zaitsev <ststas@yandex.ru>
2023-03-17 19:07:14 +0200

    feat: popup open/close updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,68 @@
+
+//определяем массив для заполенения первых 6-ти карточек
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+//определяем переменные для заполнения карточек из массива
+// const cardElements = document.querySelector('.elements');
+// const cardElement = document.querySelector('.element');
+
+// const htmlElement = cardElement.cloneNode(true);
+// console.log(htmlElement);
+// debugger;
+
+// const imageElement = htmlElement.content.querySelector('.element__image');
+// console.log(imageElement);
+// cardElements.append(htmlElement.content);
+
+
+
+// initialCards.forEach(addInitialCards)
+
+// function addInitialCards (item) {
+//   const htmlElement = cardElement.cloneNode(true);
+//   // htmlElement.querySelector('.element__image').src = 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg';
+//   // htmlElement.querySelector('.element__title').textContent = 'ТЕКСТ';
+//   cardElements.append(htmlElement);
+// };
+// addInitialCards();
+
+
+
+
+
+
+
 // определяем переменные для открытия/закрытия всплывающего окна и отправки формы
 const popupElement = document.querySelector('.popup');
 const profileElement = document.querySelector('.profile');
 const profileEditButtonElement = profileElement.querySelector('.profile__edit-button');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+
 // const popupSubmitButtonElement = popupElement.querySelector('.popup__submit-button');
 
 // определяем перемененные для имени и профессии в профиле
