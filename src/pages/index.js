71cb8a2afc4a8c27c4179f7a3471cf04dbb83061
Stas Zaitsev <ststas@yandex.ru>
2023-05-30 23:19:34 +0300

    user id updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -180,7 +180,7 @@ profileAddButtonElement.addEventListener('click', function () {
 Promise.all([api.getUserInfo(), api.getInitialCards()])
   .then(([userData, cardsData]) => {
     profileInfo.setUserInfo(userData);
-    cardRenderer(cardsData, profileInfo.getUserId(), 'append');
+    cardRenderer(cardsData, userData._id, 'append');
   })
   .catch(err => console.error(`Ошибка загрузки данных с сервера: ${err}`))
   
